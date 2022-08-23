77fecc78add10066d5130446976dc67939d46e58
wiseaidev <business@wiseai.dev>
2022-08-23 22:12:40 +0300

    update Helper
    Signed-off-by: wiseaidev <business@wiseai.dev>

--- src/components/Helper/index.js
+++ src/components/Helper/index.js
@@ -15,6 +15,35 @@ const componentColors = [
   "secondary",
 ];
 
+const linkify = (inputText) => {
+  let replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+  //URLs starting with http://, https://, or ftp://
+  replacePattern1 =
+    /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\\/%?=~_|!:,.;]*[-A-Z0-9+&@#\\/%=~_|])/gim;
+  replacedText = inputText.replace(
+    replacePattern1,
+    '<a href="$1" target="_blank">$1</a>'
+  );
+
+  //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+  replacePattern2 = /(^|[^\\/])(www\.[\S]+(\b|$))/gim;
+  replacedText = replacedText.replace(
+    replacePattern2,
+    '$1<a href="http://$2" target="_blank">$2</a>'
+  );
+
+  //Change email addresses to mailto:: links.
+  replacePattern3 =
+    /(([a-zA-Z0-9\-_.])+@[a-zA-Z0-9\\-]+?(\.[a-zA-Z]{2,6})+)/gim;
+  replacedText = replacedText.replace(
+    replacePattern3,
+    '<a href="mailto:$1">$1</a>'
+  );
+
+  return replacedText;
+};
+
 const PhoneNumberInput = React.forwardRef(
   ({ onChange, value, ...other }, ref) => {
     const [phoneNumber, setPhoneNumber] = useState("");
@@ -46,4 +75,9 @@ const PhoneNumberInput = React.forwardRef(
 const isValidEmail = (value) => {
   return value && /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,8}$/i.test(value);
 };
-export { componentColors, isValidEmail, PhoneNumberInput };
+export {
+  componentColors,
+  isValidEmail,
+  PhoneNumberInput,
+  linkify,
+};
