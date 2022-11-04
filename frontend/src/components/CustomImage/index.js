10dc7c34a65862acbc324bd1b18f2b6a704c3723
wiseaidev <business@wiseai.dev>
2022-11-04 17:55:39 +0200

    update docs
    Signed-off-by: wiseaidev <business@wiseai.dev>

--- frontend/src/components/CustomImage/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const CustomImage = ({ alt, ...restProps }) => {
-  const altValue = alt ? alt : "";
-  return <img alt={altValue} {...restProps} />;
-};
-
-export default CustomImage;
--- frontend/src/pages/Register/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import SignUp from "../../components/SignUp";
-
-const Register = () => <SignUp />;
-
-export default Register;
